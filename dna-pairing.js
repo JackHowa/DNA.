8c319a22a19ca57623e48f2707a3df72e42de9d6
jackhowa <jackhowaa@gmail.com>
2017-12-17 13:03:57 -0600

    Use a spread and a switch statement to match letters

--- dna-pairing.js
+++ dna-pairing.js
@@ -1,6 +1,26 @@
 function pairElement(str) {
     const singlesArray = [...str]; // use es2015 spread [ 'G', 'C', 'G' ]
-    return singlesArray.map(start => [...start]); // es2015 spread with map [ [ 'G' ], [ 'C' ], [ 'G' ] ]
+    const singles2dArray = singlesArray.map(start => [
+        ...start,
+        findMatch(start)
+    ]); // es2015 spread with map [ [ 'G' ], [ 'C' ], [ 'G' ] ]
+    return singles2dArray;
+}
+
+function findMatch(letter) {
+    switch (letter) {
+        case "A":
+            return "T";
+        case "T":
+            return "A";
+        case "G":
+            return "C";
+        case "C":
+            return "G";
+        default:
+            console.error("Please input proper genes");
+            break;
+    }
 }
 
 console.log(pairElement("GCG"));
